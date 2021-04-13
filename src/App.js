8a9ce74a8db64d0f563b55206ca8286a0d3a28a5
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-04-13 23:37:46 +0600

    top navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
  
 import './App.css';
  
+import TopNavigation from './components/TopNavigation/TopNavigation'
 import TopBanner from './components/TopBanner/TopBanner'
 import Services from './components/Services/Services'
 function App() {
   return (
     <div>
+      <TopNavigation/>
       <TopBanner/>
       <Services/>
        
---
+++ src/components/TopNavigation/TopNavigation.js
@@ -0,0 +1,50 @@
+import React, { Component,Fragment } from 'react'
+import { Navbar,Nav } from 'react-bootstrap' 
+
+export class TopNavigation extends Component {
+  constructor(){
+      super();
+      this.state = {
+          navBarTitle: "navTitle"
+      }
+  }
+
+  onScroll=() =>{
+      if(window.scrollY > 100){
+          this.setState({navBarTitle: 'navTitleScroll'});
+      }else if(window.scrollY < 100){
+        this.setState({navBarTitle: 'navTitle'});
+      }
+  }
+
+  componentDidMount(){
+      window.addEventListener('scroll', this.onScroll);
+  }
+    render() {
+        return (
+            <Fragment>
+                <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Navbar.Brand className={this.state.navBarTitle} href="#home">Rafiqul Islam</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto">
+                        
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">HOME</Nav.Link>
+                            <Nav.Link href="#deets">SERVICES</Nav.Link>
+                            <Nav.Link href="#deets">COURSES</Nav.Link>
+                            <Nav.Link href="#deets">PORTFOLIO</Nav.Link>
+                            <Nav.Link href="#deets">CONTACT</Nav.Link>
+                            <Nav.Link href="#deets">ABOUT</Nav.Link>
+                            
+                       
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </Fragment>
+        )
+    }
+}
+
+export default TopNavigation
