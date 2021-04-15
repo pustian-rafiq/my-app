70ca680de8d4cd5238ae0a1a4ecd05c05fc273d8
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-04-15 16:43:39 +0600

    course section complete

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Services from './components/Services/Services'
 import Analysis from './components/Analysis/Analysis'
 import Summary from './components/Summary/Summary';
 import RecentProjects from './components/RecentProjects/RecentProjects';
+import Courses from './components/Courses/Courses';
 function App() {
   return (
     <div>
@@ -16,6 +17,7 @@ function App() {
       <Analysis/>
       <Summary/>
       <RecentProjects/>
+      <Courses/>
 
        
     </div>
