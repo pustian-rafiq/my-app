f42fbd9a1fc89108a4e54b7e08668ebbdd370293
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-04-22 15:40:29 +0600

    dynamic service section

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -2,37 +2,45 @@ import React, { Component } from 'react'
 import { Fragment } from 'react'
 import { Container,Row,Col } from 'react-bootstrap'
 import development from '../../asset/images/development.png'
-import mobile from '../../asset/images/mobile.png'
-import graphicslogo from '../../asset/images/graphicslogo.png'
- 
+// import mobile from '../../asset/images/mobile.png'
+// import graphicslogo from '../../asset/images/graphicslogo.png'
+import RestClient from '../../RestAPI/RestClient';
+import AppUrl from '../../RestAPI/AppUrl';
+
 class Serivces extends Component {
+
+    constructor(){
+        super();
+        this.state ={
+            servicesData: []
+        }
+    }
+        componentDidMount(){
+            RestClient.GetRequest(AppUrl.ServicesData).then(result =>{
+                this.setState({servicesData: result});
+            })
+        }
+
     render() {
+
+        const myList = this.state.servicesData;
+       const servicesDatView = myList.map(myList=>{
+
+            return <Col lg={4} md={6} sm={12}>
+                            <div className="serviceCard text-center">
+                                <img src={development} alt="web logo"/> 
+                                <h2 className="serviceName">{myList.service_name}</h2>
+                                <p className="serviceDescription">{myList.service_description}</p>
+                            </div>
+                        </Col>
+        })
         return (
             <Fragment>
                 <Container className="text-center">
                     <h1 className="serviceMainTitle">MY SERVICES</h1>
                     <Row>
-                        <Col lg={4} md={6} sm={12}>
-                            <div className="serviceCard text-center">
-                                <img src={development} alt="web logo"/> 
-                                <h2 className="serviceName">Web Development</h2>
-                                <p className="serviceDescription">Carousels don’t automatically normalize slide dimensions. As such, you may need to use additional utilities or custom styles to appropriately size content.</p>
-                            </div>
-                        </Col>
-                         <Col lg={4} md={6} sm={12}>
-                            <div className="serviceCard text-center">
-                                <img src={mobile} alt="mobile logo"/> 
-                                <h2 className="serviceName">Mobile Development</h2>
-                                <p className="serviceDescription">Carousels don’t automatically normalize slide dimensions. As such, you may need to use additional utilities or custom styles to appropriately size content.</p>
-                            </div>
-                        </Col>
-                         <Col lg={4} md={6} sm={12}>
-                            <div className="serviceCard text-center">
-                                <img src={graphicslogo} alt="graphics logo"/> 
-                                <h2 className="serviceName">Graphics Design</h2>
-                                <p className="serviceDescription">Carousels don’t automatically normalize slide dimensions. As such, you may need to use additional utilities or custom styles to appropriately size content.</p>
-                            </div>
-                        </Col>
+                       
+                       {servicesDatView}
 
                     </Row>
 
