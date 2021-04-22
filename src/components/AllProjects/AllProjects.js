9a9dc64a3c3449224462273cac7c49a80a084efb
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-04-22 16:41:58 +0600

    dynamic all project and portfolio

--- src/components/AllProjects/AllProjects.js
+++ src/components/AllProjects/AllProjects.js
@@ -1,103 +1,50 @@
 import React, { Component,Fragment } from 'react'
 import  { Container,Row,Col,Card,Button } from 'react-bootstrap'
-import laptop from '../../asset/images/laptop.png'
+// import laptop from '../../asset/images/laptop.png'
 import {Link} from 'react-router-dom'
-
+import RestClient from '../../RestAPI/RestClient';
+import AppUrl from '../../RestAPI/AppUrl';
 
 class AllProjects extends Component {
+    constructor(){
+        super();
+        this.state ={
+            projectsData: []
+        }
+    }
+        componentDidMount(){
+            RestClient.GetRequest(AppUrl.AllProject).then(result =>{
+                this.setState({projectsData: result});
+            })
+        }
+
+
     render() {
+
+        const myList = this.state.projectsData;
+        const projectDataView = myList.map(myList=>{
+ 
+             return <Col lg={4} md={6} sm={12}>
+                        <Card className="projectCard">
+                            <Card.Img variant="top" src={myList.image_one} />
+                            <Card.Body>
+                                <Card.Title className="projectCardTitle">{myList.project_title}</Card.Title>
+                                <Card.Text className="projectCardDescription">{myList.project_description} </Card.Text>
+                                <Button variant="primary"><Link className="link-style" to="/project-details">Details</Link></Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+         });
+
+
+ 
         return (
             <Fragment>
                 <Container className="text-center mt-5">
                     
                     <Row>
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className="projectCard">
-                                <Card.Img variant="top" src={laptop} />
-                                <Card.Body>
-                                    <Card.Title className="projectCardTitle">Card Title</Card.Title>
-                                    <Card.Text className="projectCardDescription">
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><Link className="link-style"  to="/project-details">Details</Link></Button>
-                                </Card.Body>
-                             </Card>
-                        </Col>
-
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className="projectCard">
-                              <Card.Img variant="top" src={laptop} />
-                                <Card.Body>
-                                    <Card.Title className="projectCardTitle">Card Title</Card.Title>
-                                    <Card.Text className="projectCardDescription">
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><Link className="link-style"  to="/project-details">Details</Link></Button>
-                                </Card.Body>
-                             </Card>
-                        </Col>
-
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className="projectCard">
-                               <Card.Img variant="top" src={laptop} />
-                                <Card.Body>
-                                    <Card.Title className="projectCardTitle">Card Title</Card.Title>
-                                    <Card.Text className="projectCardDescription">
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><Link className="link-style"  to="/project-details">Details</Link></Button>
-                                </Card.Body>
-                             </Card>
-                        </Col>
-
-                        
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className="projectCard">
-                               <Card.Img variant="top" src={laptop} />
-                                <Card.Body>
-                                    <Card.Title className="projectCardTitle">Card Title</Card.Title>
-                                    <Card.Text className="projectCardDescription">
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><Link className="link-style"  to="/project-details">Details</Link></Button>
-                                </Card.Body>
-                             </Card>
-                        </Col>
-
-                        
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className="projectCard">
-                               <Card.Img variant="top" src={laptop} />
-                                <Card.Body>
-                                    <Card.Title className="projectCardTitle">Card Title</Card.Title>
-                                    <Card.Text className="projectCardDescription">
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><Link className="link-style" to="/project-details">Details</Link></Button>
-                                </Card.Body>
-                             </Card>
-                        </Col>
-
-                        
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className="projectCard">
-                               <Card.Img variant="top" src={laptop} />
-                                <Card.Body>
-                                    <Card.Title className="projectCardTitle">Card Title</Card.Title>
-                                    <Card.Text className="projectCardDescription">
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><Link className="link-style"  to="/project-details">Details</Link></Button>
-                                </Card.Body>
-                             </Card>
-                        </Col>
-
+                       {projectDataView}
                     </Row>
                     
                 </Container>
--- src/components/ClientReview/ClientReview.js
+++ src/components/ClientReview/ClientReview.js
@@ -3,7 +3,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { Container,Row,Col } from 'react-bootstrap'
-import rafiq from '../../asset/images/rafiq.jpg'
+// import rafiq from '../../asset/images/rafiq.jpg'
 import RestClient from '../../RestAPI/RestClient';
 import AppUrl from '../../RestAPI/AppUrl';
 
